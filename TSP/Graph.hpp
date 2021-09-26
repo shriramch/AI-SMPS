7728984c7f633b74fd0c2b04f5115f8a0ae9b1fc
Shriram Chandran <shriramchandran253@gmail.com>
2021-09-26 18:08:00 +0530

    Add runGenetic

--- TSP/Graph.hpp
+++ TSP/Graph.hpp
@@ -1,19 +1,25 @@
-#ifndef AI_SMPS_GRAPH_H
-#define AI_SMPS_GRAPH_H
+#ifndef GRAPH_H
+#define GRAPH_H
 
 #include <bits/stdc++.h>
 
-// class Graph {
-//   vector<vector<double>> adj;
-//   vector<pair<double, double>> cds;
-//  public:
-//   Graph();
-//   Graph(int);
-//   void addNode(double, double)
-//   void addEdge(double, double, double);
-// };
 using namespace std;
 
-void cycle_crossover(vector<int> &p1, vector<int> &p2, vector<int> &c1, vector<int> &c2);
+class Graph {
+  vector<vector<double>> adj;
+  vector<pair<double, double>> cds;
+ public:
+  Graph();
+  Graph(int);
+  void addNode(double, double)
+  void addEdge(double, double, double);
+  int getN();
+  double tourCost(vector<int> &);
+};
 
-#endif //AI_SMPS_GRAPH_H
+void generate_cycles(int, int, vector<vector<int>> &);
+void cycle_crossover(vector<int> &, vector<int> &, vector<int> &, vector<int> &);
+void newGeneration(vector<vector<int>> &, vector<vector<int>> &);
+void mix(vector<vector<int>> &, vector<vector<int>> &);
+
+#endif //GRAPH_H
