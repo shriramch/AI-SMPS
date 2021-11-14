936b5ad5f3719acc064801ce6827bcbde0b64ad2
Shriram Chandran <shriramchandran253@gmail.com>
2021-11-14 11:58:11 +0530

    Added iterative depth

--- Desdemona/bots/MyBot/MyBot.cpp
+++ Desdemona/bots/MyBot/MyBot.cpp
@@ -9,11 +9,13 @@
 #include "OthelloBoard.h"
 #include "OthelloPlayer.h"
 #include <cstdlib>
-#define MAX_DEPTH 6
+#include <ctime>
+
 #define INF 1e18
 #define ld long double
 using namespace std;
 using namespace Desdemona;
+#define TL 2
 
 ld min(ld &a, ld &b) {
     if (a < b) return a; else return b;
@@ -22,6 +24,9 @@ ld min(ld &a, ld &b) {
 ld max(ld &a, ld &b) {
     if (a > b) return a; else return b;
 }
+
+clock_t start, finish;
+int max_depth = 0;
 
 class MyBot: public OthelloPlayer
 {
@@ -64,41 +69,82 @@ ld eval(Move move, OthelloBoard board, Turn turn){
     return m;
 }
 
-ld alpha_beta(OthelloBoard board, int depth, Move move, Turn turn, ld alpha, ld beta) {
-    if (depth == MAX_DEPTH) {
-        return eval(move);
+ld alpha_beta(OthelloBoard board, int depth, Move move, Turn turn, ld alpha,
+              ld beta) {
+  if ((ld)(clock() - start) / CLOCKS_PER_SEC > 0.99 * TL) {
+      if (depth & 1) {
+          return -INF;
+      } else {
+          return INF;
+      }
+  }
+  while (true) {
+    if (depth == max_depth) {
+      return eval(move, board, turn);
     } else {
-        board.makeMove(turn, move);
-        turn = other(turn);
-        list<Move> moves = board.getValidMoves(turn);
-        ld res = -INF;
-        if (depth & 1) {
-            for (auto nextMove: moves) {
-                beta = min(beta, alpha_beta(board, depth, nextMove, turn, alpha, beta));
-                if (alpha >= beta) {
-                    return alpha;
-                }
-            }
-            return beta;
-        } else {
-            for (auto nextMove: moves) {
-                alpha = max(alpha, alpha_beta(board, depth, nextMove, turn, alpha, beta));
-                if (alpha >= beta) {
-                    return beta;
-                }
-            }
+      board.makeMove(turn, move);
+      turn = other(turn);
+      list<Move> moves = board.getValidMoves(turn);
+      if (moves.empty()) {
+          if (depth & 1) {
+              return -INF;
+          } else {
+              return INF;
+          }
+      }
+      if (depth & 1) {
+        for (auto nextMove : moves) {
+          beta =
+              min(beta, alpha_beta(board, depth, nextMove, turn, alpha, beta));
+          if (alpha >= beta) {
             return alpha;
+          }
         }
+        return beta;
+      } else {
+        for (auto nextMove : moves) {
+          alpha =
+              max(alpha, alpha_beta(board, depth, nextMove, turn, alpha, beta));
+          if (alpha >= beta) {
+            return beta;
+          }
+        }
+        return alpha;
+      }
     }
+  }
 }
 
 Move MyBot::play( const OthelloBoard& board )
 {
+    start = clock();
     list<Move> moves = board.getValidMoves( turn );
     int randNo = rand() % moves.size();
     list<Move>::iterator it = moves.begin();
-
-    return *it;
+    max_depth = 2;
+    Move best_of_best = *it;
+    ld best_val = -INF;
+    while(true) {
+        Move best_move = *it;
+        ld best = -INF;
+        ld alpha = -INF, beta = +INF;
+        for (auto move: moves) {
+            if ((ld)(clock() - start) / CLOCKS_PER_SEC > 0.99 * TL) {
+                break;
+            }
+            ld eval = alpha_beta(board, 0, move, turn, alpha, beta);
+            if (eval > best) {
+                eval = best;
+                best_move = move;
+            }
+        }
+        if (best > best_val) {
+            best_of_best = best_move;
+            best_val = best;
+        }
+        max_depth += 1;
+    }
+    return best_of_best;
 }
 
 // The following lines are _very_ important to create a bot module for Desdemona
